ac9cbcb9013f70748a096f2e5711b914293e5b74
michell179708 <her20045@byui.edu>
2021-02-10 17:57:10 -0600

    initial commit

--- Lesson5/js/anuncio.js
+++ Lesson5/js/anuncio.js
@@ -1,13 +1,11 @@
-window.onload = function toggleAnnouncement() {
-
-  
+window.onload = function(){
+  toggleAnnouncement();
+  function toggleAnnouncement() {  
     let d = new Date();
     let q = d.getDay();
-  
-    if (q==5) {
-      document.getElementById("pancakes").classList.toggle("hide");
+    console.log(q);
+    if (q!=5) {
+      document.getElementById("active").classList.toggle("hide");
     }
-    //A test to make sure it worked
-    //   console.log(q);
   };
-  
+}
