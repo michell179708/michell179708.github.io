25143fcd1260ada72c17ae9b5946c1b17a47d8b8
michell179708 <her20045@byui.edu>
2021-02-13 04:59:55 -0600

    initial commit

---
+++ Lesson6/js/anuncio.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+  toggleAnnouncement();
+  function toggleAnnouncement() {  
+    let d = new Date();
+    let q = d.getDay();
+    console.log(q);
+    if (q!=5) {
+      document.getElementById("active").classList.toggle("hide");
+    }
+  };
+}
