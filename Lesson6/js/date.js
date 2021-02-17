3d30ed346cc3cd54b7a634fb351ec960991bc113
michell179708 <her20045@byui.edu>
2021-02-16 18:50:12 -0600

    initial commit

--- Lesson6/js/date.js
+++ Lesson6/js/date.js
@@ -6,32 +6,31 @@ let daynames = [
     "Thursday",
     "Friday",
     "Saturday",
-
-   ];
-   let months = [
+  ];
+  let months = [
     "January",
     "February",
     "March",
     "April",
     "May",
     "June",
-    "July",   
+    "July",
     "August",
     "September",
     "October",
     "November",
-   "December",
-   ];
-   let d = new Date();
-   let dayName = daynames[d.getDay()];
-   let monthName = months[d.getMonth()];
-   let fulldate =
-   dayName +
-   ", " +
-   monthName +
-   " " +
-   d.getDate() +
-   ", " +
-   d.getFullYear();
-
-   document.getElementById("date").textContent= fulldate;
+    "December",
+  ];
+  let d = new Date();
+  let dayName = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let fulldate =
+    dayName +
+    ", " +
+    monthName +
+    " " +
+    d.getDate() +
+    ", " +
+    d.getFullYear();
+  
+  document.getElementById("date").textContent = fulldate;
--- Lesson6/js/windChill.js
+++ Lesson6/js/windChill.js
@@ -1,18 +1,18 @@
 
   
-    let tempF =parseFloat(document.getElementById("tempF").textContent);
-    let speed =parseFloat(document.getElementById("speed").textContent);
+    let tempF =parseFloat(document.getElementById("tempF").innerHTML);
+    let speed =parseFloat(document.getElementById("speed").innerHTML);
 
-    if(tempF<=50 && speed>=3){
+    if(tempF<=50 && speed>=4.8){
       let f = ( 35.74 + (0.6215 * tempF) - (35.75 * Math.pow (speed,0.16)) + (0.4275 * tempF * Math.pow(speed,0.16)) ) 
        
-      document.getElementById("f").textContent=f.toFixed(2) + "&deg; " + "F";
+      document.getElementById("f").innerHTML=f.toFixed(2) + "&deg; " + "F";
 
     }
 
     else{
       f="Not Applicable";
-      document.getElementById("f").textContent=f;
+      document.getElementById("f").innerHTML=f;
     }
 
 
