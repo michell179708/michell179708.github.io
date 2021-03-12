d27f549919e0a50e06020f1378c54260baa1d07e
michell179708 <her20045@byui.edu>
2021-03-12 00:01:19 -0600

    initial commit

--- Lesson10/js/forecast.js
+++ Lesson10/js/forecast.js
@@ -6,30 +6,36 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject); //we use console log to check if the apiURL is correct 
+    //console.log(jsObject); //we use console log to check if the apiURL is correct 
     
     // we use ID to create information in our HTML using the array jsOBject which is our information. 
-    document.getElementById("temp-min").textContent = jsObject.main.temp_min;
-    document.getElementById("temp-max").textContent = jsObject.main.temp_max;
+    document.getElementById("speed").textContent = jsObject.wind.speed;
     document.getElementById("humidity").textContent = jsObject.main.humidity;
     document.getElementById("current-condition").textContent = jsObject.weather[0].description;
-    document.getElementById("current-temp").textContent = jsObject.main.temp;
+    document.getElementById("temp").textContent = jsObject.main.temp;
+
     
 
     //calculate the windchild
-    let f;
-    let tempF = jsObject.main.temp;
-    let speedW = jsObject.wind.speed;
-
-    if(tempF<=50 && speedW>3 ){
-       f=( 35.74 + (0.6215 * tempF) - (35.75 * Math.pow (speedW,0.16)) + (0.4275 * tempF * Math.pow(speedW,0.16)));
+    let t = parseFloat(document.getElementById("temp").innerHTML);
+    let w = parseFloat(document.getElementById("speed").innerHTML);
+     
+    if (t <= 50 && w >= 3) {
+      let f =
+        35.74 +
+        0.6215 * t -
+        35.75 * Math.pow(w, 0.16) +
+        0.4275 * t * Math.pow(w, 0.16);
+    
+      f = document.getElementById("windchill").innerHTML =
+        f.toFixed(2) + "&deg; " + "F";
+    } else {
+      f = "Not Applicable";
+    
+      document.getElementById("windchill").innerHTML = f;
+    } 
       
-      }
-      else{
-        f="N/A";
-      }
-      
-      document.getElementById("child").textContent = f.toFixed(2);
+  
     
 
   });
