00653c1ef34aa947598b4e4f31cbdbe46587509a
michell179708 <her20045@byui.edu>
2021-03-11 23:40:05 -0600

    initial commit

--- Lesson10/js/forecast.js
+++ Lesson10/js/forecast.js
@@ -6,5 +6,30 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    console.log(jsObject); //we use console log to check if the apiURL is correct 
+    
+    // we use ID to create information in our HTML using the array jsOBject which is our information. 
+    document.getElementById("temp-min").textContent = jsObject.main.temp_min;
+    document.getElementById("temp-max").textContent = jsObject.main.temp_max;
+    document.getElementById("humidity").textContent = jsObject.main.humidity;
+    document.getElementById("current-condition").textContent = jsObject.weather[0].description;
+    document.getElementById("current-temp").textContent = jsObject.main.temp;
+    
+
+    //calculate the windchild
+    let f;
+    let tempF = jsObject.main.temp;
+    let speedW = jsObject.wind.speed;
+
+    if(tempF<=50 && speedW>3 ){
+       f=( 35.74 + (0.6215 * tempF) - (35.75 * Math.pow (speedW,0.16)) + (0.4275 * tempF * Math.pow(speedW,0.16)));
+      
+      }
+      else{
+        f="N/A";
+      }
+      
+      document.getElementById("child").textContent = f.toFixed(2);
+    
+
   });
