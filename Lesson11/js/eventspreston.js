9b36910c72d477a9f2eed4f09df6f48a01d65c5e
michell179708 <her20045@byui.edu>
2021-03-18 18:12:51 -0600

    initial commit

---
+++ Lesson11/js/eventspreston.js
@@ -0,0 +1,48 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+// code for the section event ---- preston
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.table(jsonObject);  // 
+        const towns = jsonObject['towns'];
+        towns.forEach(town => {
+            if (town.name == "Preston"){
+                let event = document.createElement('section')
+                let div = document.createElement('div')
+                let h2 = document.createElement('h4');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
+
+                div.setAttribute('class', 'evento')
+                h2.textContent = 'Upcoming Events of the City ';
+                h2.setAttribute = ("class", "titulo")
+                p1.textContent = town.events[0];
+                p1.setAttribute = ("class", "eventos");
+                p2.textContent = town.events[1];
+                p2.setAttribute = ("class","eventos");
+                p3.textContent = town.events[2];
+                p3.setAttribute = ("class", "eventos");
+                p4.textContent = town.events[3];
+                p4.setAttribute = ("class", "eventos");
+
+
+              
+                div.appendChild(h2);
+                div.appendChild(p1);
+                div.appendChild(p2);
+                div.appendChild(p3);
+                div.appendChild(p4);
+                event.appendChild(div);
+
+                document.querySelector('div.events').appendChild(event);
+            }
+        });
+
+    });
+
+  
