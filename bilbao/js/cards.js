eee3fb8480be80ade34719e18fba17d42b0af9dd
michell179708 <her20045@byui.edu>
2021-04-07 11:44:51 -0600

    initial commit

--- bilbao/js/cards.js
+++ bilbao/js/cards.js
@@ -1,9 +1,33 @@
-const requestURL = "https://michell179708.github.io/bilbao/js/directory.json";
+const requestURL = 'https://michell179708.github.io/bilbao/js/directory.json';
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-  });
+.then(function (response) {
+ return response.json();
+})
+.then(function (jsonObject) {
+ console.log(jsonObject);
+
+ const data = jsonObject["directory"];
+
+ for (let i=0;i<data.length;i++){
+
+  let seccion = document.createElement("section");
+  let name = document.createElement("h4");
+  let information = document.createElement("p");
+  let location = document.createElement("p");
+  let social = document.createElement("p");
+  let URL = document.createElement("p")
+  let photo = document.createElement("photo");
+
+ if(data[i].name == 'Oliver Design'|| data[i].name == 'Hotel Tamarises' || data[i].name == 'Ros'){
+    
+  name.textContent = data[i].name;
+  location.textContent = data[i].location;
+  social.textContent = data[i].social-objective;
+
+ }
+
+}
+
+
+});
